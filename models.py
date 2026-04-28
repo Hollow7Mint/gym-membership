a515d47dd32a9cdfb89eee4d8e758f1957d10621
James Holloway <james.holloway94@gmail.com>
2026-04-28 14:48:08 +0000

    post ssh key rotation heartbeat to config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 13:48:24 UTC
+# Last sync: 2026-04-28 14:48:08 UTC
