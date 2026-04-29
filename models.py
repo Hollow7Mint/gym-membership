615e6388bcc4f07711d2e46a49d98c3d0ada6429
James Holloway <james.holloway94@gmail.com>
2026-04-29 16:51:00 +0000

    anchor cloudwatch alarm threshold time in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 15:10:30 UTC
+# Last sync: 2026-04-29 16:51:00 UTC
