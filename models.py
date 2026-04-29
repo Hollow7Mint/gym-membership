21d3be9ae7aacd8b3a7301b47df83700229fa1f0
James Holloway <james.holloway94@gmail.com>
2026-04-29 06:30:19 +0000

    seal inspector finding sync in security banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 05:38:18 UTC
+# Last sync: 2026-04-29 06:30:19 UTC
