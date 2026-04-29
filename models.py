27565e8ed521c8f795f59467a353223704d67325
James Holloway <james.holloway94@gmail.com>
2026-04-29 07:11:20 +0000

    validate bastion access time in security config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 06:30:19 UTC
+# Last sync: 2026-04-29 07:11:20 UTC
