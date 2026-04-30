b9cd58fbf273b01a28d19cab2d1e877a56dd9542
James Holloway <james.holloway94@gmail.com>
2026-04-30 18:01:58 +0000

    track guardduty threat archive in config heartbeat

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 16:57:39 UTC
+# Last sync: 2026-04-30 18:01:57 UTC
