43b7798d1e9d9e8a44c3c95ddaa991b91b0d2a9d
James Holloway <james.holloway94@gmail.com>
2026-05-01 18:08:34 +0000

    flush ec2 fleet inventory marker after scan

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 17:29:28 UTC
+# Last sync: 2026-05-01 18:08:34 UTC
