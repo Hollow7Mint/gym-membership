70a62b36149ff34e8b126a6d0cdaecd4f2357393
James Holloway <james.holloway94@gmail.com>
2026-05-01 10:31:30 +0000

    bridge postgres foreign key sync through config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 09:52:20 UTC
+# Last sync: 2026-05-01 10:31:30 UTC
