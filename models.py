f91409265be84b0e3214e3a18800d655d5e7439e
James Holloway <james.holloway94@gmail.com>
2026-05-02 02:24:42 +0000

    ship parameter store refresh record after nightly run

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 01:42:12 UTC
+# Last sync: 2026-05-02 02:24:42 UTC
