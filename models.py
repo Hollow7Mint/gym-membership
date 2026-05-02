e0a24dbddbdf0c2768984d4a54195ac342eec1ea
James Holloway <james.holloway94@gmail.com>
2026-05-02 14:16:55 +0000

    push mysql connection pool sync after refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 13:28:46 UTC
+# Last sync: 2026-05-02 14:16:55 UTC
