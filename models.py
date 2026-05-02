f247c48158e183e6b6427c6d96ae14866376e0b8
James Holloway <james.holloway94@gmail.com>
2026-05-02 08:03:26 +0000

    adjust redis ttl sync stamp after cache flush

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-02 06:44:22 UTC
+# Last sync: 2026-05-02 08:03:26 UTC
