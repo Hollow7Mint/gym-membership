cdaff042a8f0594533531a9873fab0732c757865
James Holloway <james.holloway94@gmail.com>
2026-05-03 09:51:27 +0000

    settle ssh known algorithm sync time after update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-03 08:38:18 UTC
+# Last sync: 2026-05-03 09:51:27 UTC
