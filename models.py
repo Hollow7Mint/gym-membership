b8887d5d204d60e08188fd4ab9365de8458318c7
James Holloway <james.holloway94@gmail.com>
2026-05-03 22:35:58 +0000

    flush mongodb replica lag marker for nightly check

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-03 21:27:42 UTC
+# Last sync: 2026-05-03 22:35:58 UTC
