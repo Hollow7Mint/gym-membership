c255ee8ae69ea962d66b5444e594fa6df93baa92
James Holloway <james.holloway94@gmail.com>
2026-05-05 18:41:53 +0000

    store redis eviction policy sync for cache audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-05 17:58:10 UTC
+# Last sync: 2026-05-05 18:41:53 UTC
