3e5242d2c320fd13455208fc05f8c85cfe48e33f
James Holloway <james.holloway94@gmail.com>
2026-05-05 20:20:25 +0000

    tag postgres checkpoint sync for replication trail

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-05 19:42:44 UTC
+# Last sync: 2026-05-05 20:20:25 UTC
