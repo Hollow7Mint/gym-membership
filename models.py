150a048612101c19b6e609daa8d9dd8aeef702c8
James Holloway <james.holloway94@gmail.com>
2026-05-05 22:20:00 +0000

    merge mongodb shard sync time into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-05 22:00:36 UTC
+# Last sync: 2026-05-05 22:19:59 UTC
