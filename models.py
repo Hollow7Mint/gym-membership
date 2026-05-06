c101b9ad2756190dccd1d798cb50231cb59e170b
James Holloway <james.holloway94@gmail.com>
2026-05-06 14:09:09 +0000

    run ssh certificate heartbeat for scheduled audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 14:05:10 UTC
+# Last sync: 2026-05-06 14:09:09 UTC
