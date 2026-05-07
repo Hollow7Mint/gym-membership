b33c407eb3fb30efc6c49883a56fa6590b7057f2
James Holloway <james.holloway94@gmail.com>
2026-05-07 18:06:20 +0000

    frame mysql connection limit sync in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 16:49:36 UTC
+# Last sync: 2026-05-07 18:06:20 UTC
