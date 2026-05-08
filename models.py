5000d5a2811ce98b950b65af01346111fd1941a6
James Holloway <james.holloway94@gmail.com>
2026-05-08 08:40:18 +0000

    chart autoscale sync time in deployment metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 07:59:54 UTC
+# Last sync: 2026-05-08 08:40:18 UTC
