725490802c7aad06ff94be12f2eb5c51fc27ecc7
James Holloway <james.holloway94@gmail.com>
2026-05-08 12:25:29 +0000

    post cdk bootstrap sync stamp to config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 12:04:58 UTC
+# Last sync: 2026-05-08 12:25:29 UTC
