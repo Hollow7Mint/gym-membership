76c63a0e7ae188114294d393207c75166e38c8d3
James Holloway <james.holloway94@gmail.com>
2026-05-09 19:16:29 +0000

    restamp iam permission boundary sync after review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 18:30:18 UTC
+# Last sync: 2026-05-09 19:16:28 UTC
