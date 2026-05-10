ef72660a02751470ec71d3a96d9ab57ba71014df
James Holloway <james.holloway94@gmail.com>
2026-05-10 11:50:19 +0000

    submit mongodb backup marker after maintenance

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 10:45:20 UTC
+# Last sync: 2026-05-10 11:50:18 UTC
