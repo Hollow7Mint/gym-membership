da51244c2ff91c53fece88160c390dd5bfc1d5d1
James Holloway <james.holloway94@gmail.com>
2026-05-10 22:06:36 +0000

    bump s3 bucket policy sync after review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 21:06:08 UTC
+# Last sync: 2026-05-10 22:06:36 UTC
