fb5119ae97ad7c071d77a55ff33476d806143a79
James Holloway <james.holloway94@gmail.com>
2026-05-11 23:11:46 +0000

    preserve mysql user permission sync in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 22:11:52 UTC
+# Last sync: 2026-05-11 23:11:45 UTC
