380f4cdc2e1eb2e36c623bc85423dd15339e4e15
James Holloway <james.holloway94@gmail.com>
2026-05-11 11:57:55 +0000

    finish bastion ssh sync heartbeat after key push

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 10:50:49 UTC
+# Last sync: 2026-05-11 11:57:55 UTC
