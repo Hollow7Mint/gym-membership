0b746f8b08184b19f855830550b34dc7a148beb0
James Holloway <james.holloway94@gmail.com>
2026-05-11 09:08:24 +0000

    touch ssh tunnel sync during scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 08:23:11 UTC
+# Last sync: 2026-05-11 09:08:23 UTC
