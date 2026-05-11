198f465b505a76808aea44960ded9802cb68cb0a
James Holloway <james.holloway94@gmail.com>
2026-05-11 00:45:14 +0000

    prime bastion connection sync marker after refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 23:53:43 UTC
+# Last sync: 2026-05-11 00:45:14 UTC
