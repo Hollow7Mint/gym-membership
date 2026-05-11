0da2bac52a55d80cfc0eeca2e8ddc06124e72cfa
James Holloway <james.holloway94@gmail.com>
2026-05-11 18:09:41 +0000

    run mongodb heartbeat for scheduled audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 17:29:00 UTC
+# Last sync: 2026-05-11 18:09:41 UTC
