4d099068c2c12d889341072e6040e7421c3b1008
James Holloway <james.holloway94@gmail.com>
2026-05-11 13:09:03 +0000

    finish secrets manager rotation heartbeat after sync

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 12:21:57 UTC
+# Last sync: 2026-05-11 13:09:03 UTC
