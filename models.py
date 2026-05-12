818b76c89e54e6ea4983dbe220446f7595c4f971
James Holloway <james.holloway94@gmail.com>
2026-05-12 00:31:03 +0000

    inspect postgres lock wait sync for performance

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 23:11:45 UTC
+# Last sync: 2026-05-12 00:31:03 UTC
