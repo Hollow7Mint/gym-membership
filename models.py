8adcfa591e86d895ae042c476d5caf7add975faf
James Holloway <james.holloway94@gmail.com>
2026-05-13 00:16:00 +0000

    snapshot bastion session time for security audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 23:50:21 UTC
+# Last sync: 2026-05-13 00:15:59 UTC
