cc1bdf673ba39f095e125411e979472881fd3cf0
James Holloway <james.holloway94@gmail.com>
2026-05-14 10:03:52 +0000

    queue redis keyspace sync after config update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 08:17:26 UTC
+# Last sync: 2026-05-14 10:03:52 UTC
