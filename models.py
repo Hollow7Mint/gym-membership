6b108a2cedc2aadaa78ad567981226013612d56b
James Holloway <james.holloway94@gmail.com>
2026-05-14 06:35:37 +0000

    close post-deploy heartbeat for infra pipeline

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 05:48:33 UTC
+# Last sync: 2026-05-14 06:35:37 UTC
