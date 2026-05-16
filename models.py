b80d83792477fa9328db10de1b9c6f57e9c98ea4
James Holloway <james.holloway94@gmail.com>
2026-05-16 21:39:52 +0000

    sync ssm parameter refresh after pipeline pass

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 20:24:48 UTC
+# Last sync: 2026-05-16 21:39:52 UTC
