3a4b6869262204433fa331b7e62f1f3ef139e755
James Holloway <james.holloway94@gmail.com>
2026-05-17 07:06:55 +0000

    post postgres replication heartbeat to config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 05:44:21 UTC
+# Last sync: 2026-05-17 07:06:55 UTC
