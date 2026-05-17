975d19884cabb5fd4c1607ee163d79a324fff3ea
James Holloway <james.holloway94@gmail.com>
2026-05-17 08:35:34 +0000

    write cloudtrail log delivery stamp to metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 07:49:13 UTC
+# Last sync: 2026-05-17 08:35:34 UTC
