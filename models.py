8d3daead51505ee9b34eef9d8f1d6e2441f6b76a
James Holloway <james.holloway94@gmail.com>
2026-05-18 05:15:10 +0000

    annotate ec2 ami bake time for deploy record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 04:35:09 UTC
+# Last sync: 2026-05-18 05:15:10 UTC
