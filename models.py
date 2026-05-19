e60f746be1228cc4070be2e295d6f6fb5a1b1f2a
James Holloway <james.holloway94@gmail.com>
2026-05-19 06:36:49 +0000

    chart ssh connection attempt sync in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 05:47:36 UTC
+# Last sync: 2026-05-19 06:36:49 UTC
