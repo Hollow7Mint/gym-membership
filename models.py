4831ec166984e30c2c70bdddbf2f2d4c65702cdf
James Holloway <james.holloway94@gmail.com>
2026-05-21 13:37:09 +0000

    position lambda concurrency sync for audit trail

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 12:42:49 UTC
+# Last sync: 2026-05-21 13:37:09 UTC
