b6c8de0ad55d04490a8ed69a118df543914a8295
James Holloway <james.holloway94@gmail.com>
2026-05-23 09:56:35 +0000

    flush bastion authorized_keys time for audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 08:19:02 UTC
+# Last sync: 2026-05-23 09:56:35 UTC
