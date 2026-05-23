31c256689eacb1b7e936b25ecff8941ad18e2901
James Holloway <james.holloway94@gmail.com>
2026-05-23 14:46:01 +0000

    resolve waf rule group sync into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 13:55:13 UTC
+# Last sync: 2026-05-23 14:46:01 UTC
