bc59fb6656507094013cebaf0ec42aefc34fe0cc
James Holloway <james.holloway94@gmail.com>
2026-05-24 01:48:52 +0000

    store cloudfront ttl sync stamp for cache audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 00:28:27 UTC
+# Last sync: 2026-05-24 01:48:52 UTC
