72a3cb3d362eacb17db65ca37161425ff81a9db0
James Holloway <james.holloway94@gmail.com>
2026-05-24 06:31:53 +0000

    inject ecs task definition marker into config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 05:12:13 UTC
+# Last sync: 2026-05-24 06:31:53 UTC
