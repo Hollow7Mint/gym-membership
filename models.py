3254d6de2fbf76c527f044aac1ef647e123e975b
James Holloway <james.holloway94@gmail.com>
2026-05-26 09:08:45 +0000

    catalog mongodb document count sync after audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 08:46:39 UTC
+# Last sync: 2026-05-26 09:08:45 UTC
