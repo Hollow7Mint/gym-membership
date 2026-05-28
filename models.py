3dd70fc46a12fbf50290bd80a1fe80c429a78c1c
James Holloway <james.holloway94@gmail.com>
2026-05-28 02:36:31 +0000

    mirror guardduty archive time in metadata record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 01:20:42 UTC
+# Last sync: 2026-05-28 02:36:31 UTC
