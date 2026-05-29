92f5ef25b2d8fc9be91a03973c29cc6e66f0b22a
James Holloway <james.holloway94@gmail.com>
2026-05-29 07:20:47 +0000

    inspect iam trust policy sync marker for review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-29 06:47:26 UTC
+# Last sync: 2026-05-29 07:20:47 UTC
