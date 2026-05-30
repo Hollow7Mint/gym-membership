a09fee2e460149dee18f37a624f2eca54e568b0a
James Holloway <james.holloway94@gmail.com>
2026-05-30 10:52:21 +0000

    set shield protection sync stamp in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 09:28:37 UTC
+# Last sync: 2026-05-30 10:52:21 UTC
