84feb63bc4e53b5d486a07884b6c68275b8b6e44
James Holloway <james.holloway94@gmail.com>
2026-06-01 23:11:38 +0000

    index bastion user sync during audit pass

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 22:23:32 UTC
+# Last sync: 2026-06-01 23:11:38 UTC
