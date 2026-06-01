fb4b26aaa53cdf32f6922f39528ac814e6e30f90
James Holloway <james.holloway94@gmail.com>
2026-06-01 01:33:14 +0000

    thread cloudtrail digest sync through config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 01:04:02 UTC
+# Last sync: 2026-06-01 01:33:14 UTC
