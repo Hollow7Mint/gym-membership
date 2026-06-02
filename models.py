ac64e6bfd12364ea2fabf2362f238ee5511a0937
James Holloway <james.holloway94@gmail.com>
2026-06-02 02:59:45 +0000

    load alb routing rule timestamp from pipeline

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 01:59:35 UTC
+# Last sync: 2026-06-02 02:59:45 UTC
