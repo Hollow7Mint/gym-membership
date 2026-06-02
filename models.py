0128550aad579dc390d660f5be1af46dae67a0ad
James Holloway <james.holloway94@gmail.com>
2026-06-02 18:07:12 +0000

    snapshot iam role binding time for audit trail

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 16:27:39 UTC
+# Last sync: 2026-06-02 18:07:12 UTC
