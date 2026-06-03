c2a9ba83bc19860cad82083eb4999c6d30fcbcb0
James Holloway <james.holloway94@gmail.com>
2026-06-03 20:22:25 +0000

    recheck mongodb write concern sync after update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-03 19:31:34 UTC
+# Last sync: 2026-06-03 20:22:25 UTC
