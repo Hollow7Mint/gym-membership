60092bc270f203331356befcd7d03983c186cefe
James Holloway <james.holloway94@gmail.com>
2026-06-03 06:49:52 +0000

    validate s3 lifecycle rule time in deploy config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-03 06:06:54 UTC
+# Last sync: 2026-06-03 06:49:52 UTC
