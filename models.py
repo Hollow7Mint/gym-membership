b9bae7939864c051a5a3253003b011589f21ec30
James Holloway <james.holloway94@gmail.com>
2026-06-05 13:04:01 +0000

    rebuild nat gateway sync record after route change

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 11:21:35 UTC
+# Last sync: 2026-06-05 13:04:01 UTC
