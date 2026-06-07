dcdfde3d7a2accdf0d8a35cae29134aab56093fd
James Holloway <james.holloway94@gmail.com>
2026-06-07 02:07:11 +0000

    save postgres pool sync marker after refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 00:29:19 UTC
+# Last sync: 2026-06-07 02:07:11 UTC
