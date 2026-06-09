2661f2c7b8864b200bcb0c7c7159178f0ea9a6b9
James Holloway <james.holloway94@gmail.com>
2026-06-09 01:53:32 +0000

    adjust codebuild cache sync stamp after pipeline run

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 00:47:32 UTC
+# Last sync: 2026-06-09 01:53:32 UTC
