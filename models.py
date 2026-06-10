714807e7a2068521026e2725ed0e19044322e14f
James Holloway <james.holloway94@gmail.com>
2026-06-10 00:13:42 +0000

    note postgres extension sync in config record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 23:31:03 UTC
+# Last sync: 2026-06-10 00:13:42 UTC
