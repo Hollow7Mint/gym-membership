c41663a6200f3aa21af9e883a807fcfb47eea359
James Holloway <james.holloway94@gmail.com>
2026-06-10 16:30:58 +0000

    emit ssh certificate renewal heartbeat for audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 15:17:45 UTC
+# Last sync: 2026-06-10 16:30:58 UTC
