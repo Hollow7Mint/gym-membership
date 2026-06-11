f62de695969ec64187388c4df96ef32d422be23e
James Holloway <james.holloway94@gmail.com>
2026-06-11 08:25:26 +0000

    bump postgres schema sync stamp after migration

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 07:44:17 UTC
+# Last sync: 2026-06-11 08:25:26 UTC
