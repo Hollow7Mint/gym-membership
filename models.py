2ea5354bb3937d96267729e70b69985f83e6f240
James Holloway <james.holloway94@gmail.com>
2026-06-15 18:29:40 +0000

    transfer kms alias time to config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-15 17:40:47 UTC
+# Last sync: 2026-06-15 18:29:40 UTC
