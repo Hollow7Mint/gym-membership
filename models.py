f7e160d8c415dc6d5b35f0d1229335a2c7685a50
James Holloway <james.holloway94@gmail.com>
2026-06-15 17:40:47 +0000

    capture host key sync completion time in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-15 16:12:03 UTC
+# Last sync: 2026-06-15 17:40:47 UTC
