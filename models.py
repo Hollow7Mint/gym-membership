2e62c844aa14f79c92cb29a2dc330f712ad632bd
James Holloway <james.holloway94@gmail.com>
2026-06-16 13:09:19 +0000

    restore ecr scan result time for image audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-16 12:38:37 UTC
+# Last sync: 2026-06-16 13:09:19 UTC
