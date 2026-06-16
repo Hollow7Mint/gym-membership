cbbbaff67fc5b90efa6a867652dea59b351bbbf8
James Holloway <james.holloway94@gmail.com>
2026-06-16 02:36:53 +0000

    advance ssh key trust sync into config record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-16 01:51:02 UTC
+# Last sync: 2026-06-16 02:36:53 UTC
