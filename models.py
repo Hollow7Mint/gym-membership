99c502b98df66120504034a9e561d0e1aebd384f
James Holloway <james.holloway94@gmail.com>
2026-06-19 08:04:41 +0000

    tune bastion audit log sync time for review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 06:26:23 UTC
+# Last sync: 2026-06-19 08:04:41 UTC
