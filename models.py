aadcffcc225db7610f4365ac3de5b0ff1a5e0575
James Holloway <james.holloway94@gmail.com>
2026-06-19 01:14:31 +0000

    persist tunnel config refresh in ssh banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 00:18:23 UTC
+# Last sync: 2026-06-19 01:14:31 UTC
