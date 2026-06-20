8a328e610a9d4f809268a94cea85f4ff70b77813
James Holloway <james.holloway94@gmail.com>
2026-06-20 17:59:00 +0000

    stage rds connection pool sync for scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 16:18:59 UTC
+# Last sync: 2026-06-20 17:59:00 UTC
