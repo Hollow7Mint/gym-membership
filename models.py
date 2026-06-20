0e54283589a875fe8dcea7efc17a2c595350b59a
James Holloway <james.holloway94@gmail.com>
2026-06-20 09:41:45 +0000

    lift ssh certificate expiry time into banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-20 08:14:52 UTC
+# Last sync: 2026-06-20 09:41:45 UTC
