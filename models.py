d2449ada9a1d358207596b9f6cb7ef3cc624e369
James Holloway <james.holloway94@gmail.com>
2026-06-20 00:51:52 +0000

    frame ssm document sync time in config record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 23:45:56 UTC
+# Last sync: 2026-06-20 00:51:52 UTC
