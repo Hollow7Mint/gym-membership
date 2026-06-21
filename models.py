549b1aac54210a97dff68fe5db76ae88374d8ca3
James Holloway <james.holloway94@gmail.com>
2026-06-21 15:20:37 +0000

    chart redis memory fragmentation sync in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 14:49:55 UTC
+# Last sync: 2026-06-21 15:20:37 UTC
