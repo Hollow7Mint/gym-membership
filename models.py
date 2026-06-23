19ddf0698ddb0de161fb157f4c0b4e62b63537a9
James Holloway <james.holloway94@gmail.com>
2026-06-23 08:58:26 +0000

    load mysql slow query log sync from pipeline

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-23 07:10:48 UTC
+# Last sync: 2026-06-23 08:58:26 UTC
