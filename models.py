82e9e685de07e40074d9b36bcf282cf44dc3e440
James Holloway <james.holloway94@gmail.com>
2026-06-24 08:22:01 +0000

    finish mongodb sharding sync heartbeat after balance

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-24 07:53:00 UTC
+# Last sync: 2026-06-24 08:22:01 UTC
