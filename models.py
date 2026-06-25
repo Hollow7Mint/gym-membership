2b014451d88b2e1dcff2505623f0d79ec55a991d
James Holloway <james.holloway94@gmail.com>
2026-06-25 16:39:05 +0000

    bridge lambda env var sync through config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-25 15:32:20 UTC
+# Last sync: 2026-06-25 16:39:05 UTC
