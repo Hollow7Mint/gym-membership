93365b1986e73699e6356962154c14723aaac4da
James Holloway <james.holloway94@gmail.com>
2026-06-26 17:05:02 +0000

    relay db connection string time through config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 15:16:33 UTC
+# Last sync: 2026-06-26 17:05:02 UTC
