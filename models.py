0e10447cafd058bf192cd09318ca7102c353341b
James Holloway <james.holloway94@gmail.com>
2026-06-27 21:34:51 +0000

    anchor ssh client config sync after key rotation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 20:33:26 UTC
+# Last sync: 2026-06-27 21:34:51 UTC
