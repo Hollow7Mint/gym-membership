912506515ea2236e645bae1c5807ba19df756b59
James Holloway <james.holloway94@gmail.com>
2026-07-01 12:37:27 +0000

    annotate mysql slow log sync time for audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-01 11:57:01 UTC
+# Last sync: 2026-07-01 12:37:27 UTC
