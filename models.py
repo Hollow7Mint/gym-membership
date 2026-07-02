8aa2361e3316ab518b7f3dcc9771949c898a537e
James Holloway <james.holloway94@gmail.com>
2026-07-02 10:00:47 +0000

    bridge ssh ProxyJump config sync through banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 08:26:07 UTC
+# Last sync: 2026-07-02 10:00:46 UTC
