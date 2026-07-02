d9bb452c28059737f62b0144a9060d8f61624ac4
James Holloway <james.holloway94@gmail.com>
2026-07-02 02:29:17 +0000

    capture rds snapshot completion time in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 01:29:42 UTC
+# Last sync: 2026-07-02 02:29:17 UTC
