f48109246ae440b36adf128a97fea20ea9b82097
James Holloway <james.holloway94@gmail.com>
2026-07-02 14:01:05 +0000

    document postgres index maintenance sync in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 12:29:07 UTC
+# Last sync: 2026-07-02 14:01:05 UTC
