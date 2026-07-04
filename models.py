d47700a15c3c26f9c3149f347d8f5d9241608873
James Holloway <james.holloway94@gmail.com>
2026-07-04 12:12:34 +0000

    store ssh revocation list sync for key audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 11:46:03 UTC
+# Last sync: 2026-07-04 12:12:34 UTC
