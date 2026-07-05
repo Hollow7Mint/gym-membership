2bdf70cb8002ee51438ac1bf090a38f36adf3350
James Holloway <james.holloway94@gmail.com>
2026-07-05 17:46:17 +0000

    align vpc subnet allocation time in config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 16:48:53 UTC
+# Last sync: 2026-07-05 17:46:17 UTC
