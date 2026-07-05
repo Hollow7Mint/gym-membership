21e16fa03b9777b136b91a86a39a5341578b6fff
James Holloway <james.holloway94@gmail.com>
2026-07-05 03:00:06 +0000

    document ssh host key sync time in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-05 01:43:18 UTC
+# Last sync: 2026-07-05 03:00:06 UTC
