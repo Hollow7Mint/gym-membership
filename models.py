211dc81206601231ede0e32a8589d6f4b9ebdf35
James Holloway <james.holloway94@gmail.com>
2026-07-06 06:17:37 +0000

    recheck ecs service sync after deployment window

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 05:55:47 UTC
+# Last sync: 2026-07-06 06:17:37 UTC
