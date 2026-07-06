4ba47a191b15f6be42e62f2d273fdbdb71d0fa76
James Holloway <james.holloway94@gmail.com>
2026-07-06 17:56:29 +0000

    register waf acl update time for security trail

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 17:08:19 UTC
+# Last sync: 2026-07-06 17:56:29 UTC
