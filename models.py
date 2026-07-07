d64ed862286bd9bcb91f17b6f006dd1ac01ae781
James Holloway <james.holloway94@gmail.com>
2026-07-07 06:54:00 +0000

    merge cloudformation stack apply time into record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 05:32:59 UTC
+# Last sync: 2026-07-07 06:54:00 UTC
