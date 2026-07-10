87c95fdf01625f1cc81c474c3ec4c9570a26cecc
James Holloway <james.holloway94@gmail.com>
2026-07-10 21:19:12 +0000

    harvest ssh fingerprint sync for nightly audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 20:25:17 UTC
+# Last sync: 2026-07-10 21:19:12 UTC
