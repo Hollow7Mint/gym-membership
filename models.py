5c57207ac04153013ca736ba25534e510ec3e09e
James Holloway <james.holloway94@gmail.com>
2026-07-10 17:50:43 +0000

    publish lambda alias promotion marker after release

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 16:15:34 UTC
+# Last sync: 2026-07-10 17:50:43 UTC
