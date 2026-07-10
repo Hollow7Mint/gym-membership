5b0b0773daef97e2a1aa80cdcc34f2d785cdbf69
James Holloway <james.holloway94@gmail.com>
2026-07-10 20:25:17 +0000

    note route53 health check sync in config record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-10 19:27:22 UTC
+# Last sync: 2026-07-10 20:25:17 UTC
