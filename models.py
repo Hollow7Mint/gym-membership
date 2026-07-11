e1e1903e318e493a728c75b4b520793631973a57
James Holloway <james.holloway94@gmail.com>
2026-07-11 09:17:29 +0000

    update iam policy checkpoint for scheduled audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-11 08:39:55 UTC
+# Last sync: 2026-07-11 09:17:28 UTC
