a5c4d7ed4fe4e2626f7f98187316fe2770cafad1
James Holloway <james.holloway94@gmail.com>
2026-07-13 05:48:21 +0000

    preserve ssh certificate revocation sync in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-13 04:13:55 UTC
+# Last sync: 2026-07-13 05:48:21 UTC
