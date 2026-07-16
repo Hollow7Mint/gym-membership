21cbeb64112a17cd2b4178a508f21c7a3305bb08
James Holloway <james.holloway94@gmail.com>
2026-07-16 05:48:23 +0000

    seal postgres logical replication sync in banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 04:29:45 UTC
+# Last sync: 2026-07-16 05:48:23 UTC
