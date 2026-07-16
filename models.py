21d431652afef75988649f289fda669110ec4256
James Holloway <james.holloway94@gmail.com>
2026-07-16 21:11:58 +0000

    pin mongodb shard balancing time in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 21:04:41 UTC
+# Last sync: 2026-07-16 21:11:58 UTC
