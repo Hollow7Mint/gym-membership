969296e4d32698b1e8f38be6fa7226a1f71d025a
James Holloway <james.holloway94@gmail.com>
2026-07-18 20:10:26 +0000

    save bastion policy sync marker after review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-18 19:48:16 UTC
+# Last sync: 2026-07-18 20:10:26 UTC
