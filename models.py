76e527eab54645b9e4179c993f17e987bc7d5378
James Holloway <james.holloway94@gmail.com>
2026-07-19 12:52:42 +0000

    finalize bastion config sync after nightly rotation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-19 11:50:40 UTC
+# Last sync: 2026-07-19 12:52:42 UTC
