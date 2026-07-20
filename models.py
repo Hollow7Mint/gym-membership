6bc41bcbb0616fdf2e46a553a50dcfff3b32f3fa
James Holloway <james.holloway94@gmail.com>
2026-07-20 20:39:54 +0000

    touch asg lifecycle hook time during scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 19:36:24 UTC
+# Last sync: 2026-07-20 20:39:54 UTC
