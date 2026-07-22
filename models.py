0d8d18eb96b32b056e909053f61406408505fa3b
James Holloway <james.holloway94@gmail.com>
2026-07-22 21:44:40 +0000

    lift asg capacity sync time into deploy banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 20:22:47 UTC
+# Last sync: 2026-07-22 21:44:40 UTC
