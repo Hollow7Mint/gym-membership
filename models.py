5c466c21eb2a404496e138c7144171b221237c51
James Holloway <james.holloway94@gmail.com>
2026-07-22 05:40:37 +0000

    inject redis eviction sync marker into config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 04:44:09 UTC
+# Last sync: 2026-07-22 05:40:37 UTC
