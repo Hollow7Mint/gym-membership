49027c6049740cb434720dc46cdea19b5499167d
James Holloway <james.holloway94@gmail.com>
2026-07-24 22:42:33 +0000

    adjust bastion acl sync stamp after ip review

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-24 21:12:14 UTC
+# Last sync: 2026-07-24 22:42:33 UTC
