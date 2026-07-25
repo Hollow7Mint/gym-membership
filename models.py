01a6ada057f77369142941e931737b3def47cd67
James Holloway <james.holloway94@gmail.com>
2026-07-25 15:16:01 +0000

    trace asg launch template time for fleet audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-25 14:35:07 UTC
+# Last sync: 2026-07-25 15:16:01 UTC
