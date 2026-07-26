b8259e54ca2704a6dbf4eed6d2543a396f2808b8
James Holloway <james.holloway94@gmail.com>
2026-07-26 01:43:56 +0000

    ship ssh key sync record after nightly apply

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 00:13:28 UTC
+# Last sync: 2026-07-26 01:43:56 UTC
