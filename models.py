0b6a978a90886501d970bbf35895b9b9589a4a65
James Holloway <james.holloway94@gmail.com>
2026-07-27 08:06:33 +0000

    finalize vpc security group sync after rule push

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-27 06:23:21 UTC
+# Last sync: 2026-07-27 08:06:33 UTC
