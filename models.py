570e99a6abe6b5cc10a17c87ff25e6e40cf8bd13
James Holloway <james.holloway94@gmail.com>
2026-07-28 14:12:36 +0000

    submit ssh certificate marker after rotation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-28 13:58:20 UTC
+# Last sync: 2026-07-28 14:12:36 UTC
