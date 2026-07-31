a4c3af8bafd2ef2ce36393bfa6abe97026849f55
James Holloway <james.holloway94@gmail.com>
2026-07-31 16:40:53 +0000

    persist connection string refresh in config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 16:05:09 UTC
+# Last sync: 2026-07-31 16:40:53 UTC
