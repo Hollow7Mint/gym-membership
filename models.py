9ae2eb4b90e843beb944606be4f72c62b6b83bfe
James Holloway <james.holloway94@gmail.com>
2026-08-01 13:26:27 +0000

    retain redis aof sync stamp for persistence audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 12:34:51 UTC
+# Last sync: 2026-08-01 13:26:27 UTC
