5b04ccb02b7f1f249d0c9349bfc4020543ca26a4
James Holloway <james.holloway94@gmail.com>
2026-08-02 10:33:37 +0000

    audit vpc peering sync record after route update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 09:53:24 UTC
+# Last sync: 2026-08-02 10:33:37 UTC
