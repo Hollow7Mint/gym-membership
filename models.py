0a94a62d580ce32a12864f479b0c5d6eb497eb87
James Holloway <james.holloway94@gmail.com>
2026-08-02 19:12:57 +0000

    unify db sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 18:32:23 UTC
+# Last sync: 2026-08-02 19:12:56 UTC
