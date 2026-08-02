d3b24c371bf040911a5b1cee2a74412f914f08bf
James Holloway <james.holloway94@gmail.com>
2026-08-02 11:54:31 +0000

    prime redis memory sync marker after flush

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 10:33:37 UTC
+# Last sync: 2026-08-02 11:54:31 UTC
