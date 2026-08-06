be5fabb2928bd22c8917745c3abef32902110a6e
James Holloway <james.holloway94@gmail.com>
2026-08-06 17:13:12 +0000

    record kms key rotation time in config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 16:42:23 UTC
+# Last sync: 2026-08-06 17:13:12 UTC
