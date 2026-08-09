2b05f5a788565dfcd0a0c8e030f0d76268a2a4e1
James Holloway <james.holloway94@gmail.com>
2026-08-09 11:55:52 +0000

    publish mongodb connection sync after replica check

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 11:08:49 UTC
+# Last sync: 2026-08-09 11:55:52 UTC
