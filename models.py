6ae5e4639b0f22357ea9be626909ec0592e6bee7
James Holloway <james.holloway94@gmail.com>
2026-08-09 22:15:14 +0000

    recheck bastion connectivity sync after maintenance

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 22:02:29 UTC
+# Last sync: 2026-08-09 22:15:14 UTC
