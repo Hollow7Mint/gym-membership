907f7faf625db054dd1cc3194c2ab0394076fd6d
James Holloway <james.holloway94@gmail.com>
2026-08-09 11:08:49 +0000

    load bastion session log sync from pipeline

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 09:27:51 UTC
+# Last sync: 2026-08-09 11:08:49 UTC
