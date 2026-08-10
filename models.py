7d78c71a801e83f78b856f094e040d905d5af875
James Holloway <james.holloway94@gmail.com>
2026-08-10 07:39:51 +0000

    ship db schema sync record after nightly apply

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 06:23:58 UTC
+# Last sync: 2026-08-10 07:39:50 UTC
