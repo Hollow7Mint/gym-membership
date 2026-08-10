2b42243a6e367576b24db9e29def08900295bb8d
James Holloway <james.holloway94@gmail.com>
2026-08-10 20:15:32 +0000

    stage bastion jump host sync for scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 19:34:36 UTC
+# Last sync: 2026-08-10 20:15:32 UTC
