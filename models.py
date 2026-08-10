0b03ed2c5da9c473cd7e8db555f4d81883764f59
James Holloway <james.holloway94@gmail.com>
2026-08-10 15:05:21 +0000

    stage postgres tablespace sync for scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 13:10:31 UTC
+# Last sync: 2026-08-10 15:05:21 UTC
