4fcf5441b327b3801e2fcc729b28b3f4e474cab7
James Holloway <james.holloway94@gmail.com>
2026-08-10 10:03:14 +0000

    keep ecs task sync marker fresh after rollout

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 09:03:00 UTC
+# Last sync: 2026-08-10 10:03:14 UTC
