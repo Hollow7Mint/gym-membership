415f0c023d5d619493143ee66900337a1f8f7d6b
James Holloway <james.holloway94@gmail.com>
2026-08-11 13:53:43 +0000

    inject known_hosts sync marker into config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 12:25:08 UTC
+# Last sync: 2026-08-11 13:53:43 UTC
