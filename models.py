c15d6e4d94a73d5017a46ad60d440a5377e3339c
James Holloway <james.holloway94@gmail.com>
2026-08-13 01:43:18 +0000

    push ssh config sync marker after host refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 00:25:13 UTC
+# Last sync: 2026-08-13 01:43:18 UTC
