d754da0bb3e8929804778b1c1b5b354064ef5e52
James Holloway <james.holloway94@gmail.com>
2026-08-13 12:24:36 +0000

    set redis cluster sync stamp in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 11:57:03 UTC
+# Last sync: 2026-08-13 12:24:36 UTC
