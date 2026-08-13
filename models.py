1651e0b2e8c58f05a0cd48b5a04850ae71c23d5c
James Holloway <james.holloway94@gmail.com>
2026-08-13 10:28:18 +0000

    tune cfn change set completion stamp for audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 09:25:19 UTC
+# Last sync: 2026-08-13 10:28:18 UTC
