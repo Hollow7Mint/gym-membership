330ef584e4e50a60808e8d8fffdf299e8b64cdbc
James Holloway <james.holloway94@gmail.com>
2026-08-13 15:41:15 +0000

    preserve nat gateway sync time in infra banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 15:07:29 UTC
+# Last sync: 2026-08-13 15:41:15 UTC
