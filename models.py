07b01128c20edc33ff310725ee91fe8c9ead8b5d
James Holloway <james.holloway94@gmail.com>
2026-08-14 20:59:51 +0000

    touch redis sentinel sync during scheduled refresh

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 19:12:03 UTC
+# Last sync: 2026-08-14 20:59:51 UTC
