0bb0733a31327c9e5a1f0431c34f07eb8e0d8ade
James Holloway <james.holloway94@gmail.com>
2026-08-15 01:13:41 +0000

    resolve known_hosts cleanup sync into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 00:50:58 UTC
+# Last sync: 2026-08-15 01:13:41 UTC
