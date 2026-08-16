b2800939ea9fd55de9568a0535bf99b2c35984e8
James Holloway <james.holloway94@gmail.com>
2026-08-16 12:27:00 +0000

    write bastion access log time into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-16 12:01:58 UTC
+# Last sync: 2026-08-16 12:27:00 UTC
