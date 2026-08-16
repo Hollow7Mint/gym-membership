ef4d3d682e6d7738cfa8d80484fc67ab3e695b37
James Holloway <james.holloway94@gmail.com>
2026-08-16 04:26:32 +0000

    reload postgres autovacuum sync marker for audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-16 03:25:00 UTC
+# Last sync: 2026-08-16 04:26:32 UTC
