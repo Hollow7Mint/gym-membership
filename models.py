9e201d0fceb21128aed386e1d8d726735898deb3
James Holloway <james.holloway94@gmail.com>
2026-08-16 00:07:17 +0000

    surface codebuild log sync time for pipeline audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-15 22:36:26 UTC
+# Last sync: 2026-08-16 00:07:17 UTC
