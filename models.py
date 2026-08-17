998a8d1eb6aa82cbd95cfffacb51df134a3e6ec4
James Holloway <james.holloway94@gmail.com>
2026-08-17 13:35:09 +0000

    audit known_hosts validation time in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-17 12:54:36 UTC
+# Last sync: 2026-08-17 13:35:09 UTC
