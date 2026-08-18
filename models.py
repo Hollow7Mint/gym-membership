89654b7175ca880faf8c51977cc7c4dfe1d53f40
James Holloway <james.holloway94@gmail.com>
2026-08-18 06:39:37 +0000

    rotate kms key metadata sync after policy apply

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 05:22:49 UTC
+# Last sync: 2026-08-18 06:39:37 UTC
