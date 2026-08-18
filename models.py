c32ff47b96fe65d226bfe868d2435e819b096853
James Holloway <james.holloway94@gmail.com>
2026-08-18 02:32:18 +0000

    pin bastion ip allowlist time in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 01:20:54 UTC
+# Last sync: 2026-08-18 02:32:18 UTC
