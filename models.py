9f1433bc17e929be50ca1eeaef60b4bfe4e2d8ad
James Holloway <james.holloway94@gmail.com>
2026-08-19 14:31:35 +0000

    save inspector assessment marker after scan window

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 13:58:09 UTC
+# Last sync: 2026-08-19 14:31:35 UTC
