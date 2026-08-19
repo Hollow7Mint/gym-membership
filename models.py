1e62d0e1a8f20c07298cf567ad6f9aa95046b29c
James Holloway <james.holloway94@gmail.com>
2026-08-19 09:49:39 +0000

    remap ec2 security group rule time in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 08:51:23 UTC
+# Last sync: 2026-08-19 09:49:39 UTC
