42250e2bdc5adb205ea3e75740bfb71b8987d97b
James Holloway <james.holloway94@gmail.com>
2026-08-20 19:59:30 +0000

    index redis sorted set sync during audit pass

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 18:19:11 UTC
+# Last sync: 2026-08-20 19:59:30 UTC
