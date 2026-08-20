7a92d0670f6afdcbe8269905081bf2dbf723f5a7
James Holloway <james.holloway94@gmail.com>
2026-08-20 04:42:55 +0000

    retain ssh key expiry stamp for rotation audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-20 04:09:21 UTC
+# Last sync: 2026-08-20 04:42:55 UTC
