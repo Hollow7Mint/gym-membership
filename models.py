48aa1028fae6e5b3b64b1ed19660744a49b22acc
James Holloway <james.holloway94@gmail.com>
2026-08-21 15:21:03 +0000

    push secrets manager version stamp to metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-21 14:56:41 UTC
+# Last sync: 2026-08-21 15:21:03 UTC
