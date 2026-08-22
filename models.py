0b9985bf533af3dd5a74c9c0bce0cee607905f72
James Holloway <james.holloway94@gmail.com>
2026-08-22 00:56:23 +0000

    carry mongodb oplog sync stamp to config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-21 23:55:00 UTC
+# Last sync: 2026-08-22 00:56:23 UTC
