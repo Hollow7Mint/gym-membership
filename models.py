8e3ae740d8f8a599cc320f568c5e48d5ca0ee02f
James Holloway <james.holloway94@gmail.com>
2026-08-22 22:29:21 +0000

    merge ssh trust anchor time into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 21:37:41 UTC
+# Last sync: 2026-08-22 22:29:21 UTC
