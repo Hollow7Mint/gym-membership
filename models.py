8ccf2788d4619928e03557d5de5df9d617eabf63
James Holloway <james.holloway94@gmail.com>
2026-08-23 15:30:33 +0000

    anchor mysql parameter sync after maintenance window

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 14:58:17 UTC
+# Last sync: 2026-08-23 15:30:32 UTC
