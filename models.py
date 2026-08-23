39261f04d9de81340fa2f3ccb82d33d475f1e8e5
James Holloway <james.holloway94@gmail.com>
2026-08-23 21:48:31 +0000

    refresh aws config sync marker after nightly reconcile

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 20:35:31 UTC
+# Last sync: 2026-08-23 21:48:31 UTC
