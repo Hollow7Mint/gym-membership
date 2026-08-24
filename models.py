43594dfa5fa1d560275dd2865003273d9c4d5823
James Holloway <james.holloway94@gmail.com>
2026-08-24 10:46:11 +0000

    harvest ecr image push time for nightly audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-24 09:15:19 UTC
+# Last sync: 2026-08-24 10:46:11 UTC
