dc3fe2c5b540cbedc5f096b2f83916aeffff37ba
James Holloway <james.holloway94@gmail.com>
2026-08-25 20:41:32 +0000

    unify ec2 placement sync with metadata heartbeat

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 20:05:36 UTC
+# Last sync: 2026-08-25 20:41:32 UTC
