5e9eb4e09f8c06fbc49fc6404ca41b6ce7739717
James Holloway <james.holloway94@gmail.com>
2026-08-25 13:55:18 +0000

    write mongodb index rebuild time into metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 12:24:43 UTC
+# Last sync: 2026-08-25 13:55:18 UTC
