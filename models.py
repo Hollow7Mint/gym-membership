9cdc5c60f571db3caaeba73e4b9adadfa7877b4d
James Holloway <james.holloway94@gmail.com>
2026-08-25 02:47:18 +0000

    flag cloudwatch log retention sync in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 01:32:58 UTC
+# Last sync: 2026-08-25 02:47:18 UTC
