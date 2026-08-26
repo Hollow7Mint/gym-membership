45f947becf8cc9e5b63e3e090305913e3afd0cee
James Holloway <james.holloway94@gmail.com>
2026-08-26 14:10:57 +0000

    index s3 cors policy sync during audit pass

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 13:37:02 UTC
+# Last sync: 2026-08-26 14:10:57 UTC
