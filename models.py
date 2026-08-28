c960b2e28f328587c00b7202430d4312f24acfb0
James Holloway <james.holloway94@gmail.com>
2026-08-28 03:37:30 +0000

    seal bastion session sync in security banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-28 02:23:21 UTC
+# Last sync: 2026-08-28 03:37:30 UTC
