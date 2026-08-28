4ce393bd4fb108b13fa0037041b894205bc270c4
James Holloway <james.holloway94@gmail.com>
2026-08-28 02:23:21 +0000

    queue cloudfront distribution stamp after invalidation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-28 01:20:10 UTC
+# Last sync: 2026-08-28 02:23:21 UTC
