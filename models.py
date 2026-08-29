88d704979c37fcdf91b59ff7c14028faff1f330a
James Holloway <james.holloway94@gmail.com>
2026-08-29 01:17:41 +0000

    bump known_hosts update stamp after rotation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 01:08:27 UTC
+# Last sync: 2026-08-29 01:17:40 UTC
