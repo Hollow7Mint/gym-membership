1ab92aa7b531582dd1c9fc72665e2ec084201b31
James Holloway <james.holloway94@gmail.com>
2026-08-29 07:10:51 +0000

    snapshot mysql instance time for database audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 06:22:18 UTC
+# Last sync: 2026-08-29 07:10:50 UTC
