7b3c9d4bed9974813c4cae61cd83be609efbd1e3
James Holloway <james.holloway94@gmail.com>
2026-08-29 23:14:32 +0000

    restore ssh public key time for rotation audit

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 22:48:57 UTC
+# Last sync: 2026-08-29 23:14:31 UTC
