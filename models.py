bb24a8df2c623db1b65d6a95d897fef035dc200c
James Holloway <james.holloway94@gmail.com>
2026-08-30 21:26:06 +0000

    inspect bastion port forward sync for security

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 21:03:11 UTC
+# Last sync: 2026-08-30 21:26:06 UTC
