4ccc6f8f626f3a753d6ab248131913a209b9774b
James Holloway <james.holloway94@gmail.com>
2026-09-02 06:14:58 +0000

    document alb target health sync for nightly check

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 05:41:12 UTC
+# Last sync: 2026-09-02 06:14:58 UTC
