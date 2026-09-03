9c30bec19fae6186c577a66d9380163db7ec20fc
James Holloway <james.holloway94@gmail.com>
2026-09-03 10:15:29 +0000

    mirror redis cluster node time in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 09:58:01 UTC
+# Last sync: 2026-09-03 10:15:29 UTC
