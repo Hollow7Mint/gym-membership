fbc9320abdadabd89cabf29f6bd4b9d1b0906ebf
James Holloway <james.holloway94@gmail.com>
2026-09-03 17:35:19 +0000

    capture db backup window completion time in record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 16:59:38 UTC
+# Last sync: 2026-09-03 17:35:19 UTC
