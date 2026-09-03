2e339fe1a53d83a8ff4d35367be3a2df73d47e4e
James Holloway <james.holloway94@gmail.com>
2026-09-03 09:58:01 +0000

    catalog rds parameter group sync after maintenance

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 08:44:38 UTC
+# Last sync: 2026-09-03 09:58:01 UTC
