479ec01a1a9eeeba937cc5e9ed18f7830e8b8582
James Holloway <james.holloway94@gmail.com>
2026-09-04 07:47:43 +0000

    thread postgres wal sync through config banner

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 06:19:48 UTC
+# Last sync: 2026-09-04 07:47:43 UTC
