84fbe942bfa3e6f3c7370df1dff2885811ff7144
James Holloway <james.holloway94@gmail.com>
2026-09-06 00:19:20 +0000

    lift db replica sync time into banner record

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-05 23:14:09 UTC
+# Last sync: 2026-09-06 00:19:20 UTC
