9ec8823b60f1fe06fa6e4c8450e2c88594c20baa
James Holloway <james.holloway94@gmail.com>
2026-09-06 07:35:35 +0000

    advance cdk app deploy time into config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-06 06:19:18 UTC
+# Last sync: 2026-09-06 07:35:35 UTC
