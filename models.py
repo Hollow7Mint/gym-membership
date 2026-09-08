7e3389c4b5dcae724ef58cb15c16d12f55b48bf5
James Holloway <james.holloway94@gmail.com>
2026-09-08 00:41:18 +0000

    carry waf ip set sync stamp to config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 00:09:12 UTC
+# Last sync: 2026-09-08 00:41:18 UTC
