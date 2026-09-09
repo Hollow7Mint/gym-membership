e2160b57470f93d2a8ff720c487b0b74e336960a
James Holloway <james.holloway94@gmail.com>
2026-09-09 15:56:32 +0000

    reconcile cloudwatch dashboard sync after update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 14:41:40 UTC
+# Last sync: 2026-09-09 15:56:32 UTC
