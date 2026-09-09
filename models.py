39101b71bc63c5a1ddce3e162a8754806fecb833
James Holloway <james.holloway94@gmail.com>
2026-09-09 11:11:28 +0000

    pin route53 dns sync stamp after record update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 10:53:50 UTC
+# Last sync: 2026-09-09 11:11:28 UTC
