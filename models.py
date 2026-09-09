1621ea0cd951bfb55c284ba2ee8897801974767d
James Holloway <james.holloway94@gmail.com>
2026-09-09 02:43:41 +0000

    resolve mysql backup sync into metadata file

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 01:54:29 UTC
+# Last sync: 2026-09-09 02:43:41 UTC
