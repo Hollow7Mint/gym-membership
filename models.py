9930c249017d8672d554cb8d15f81f768b83ed1b
James Holloway <james.holloway94@gmail.com>
2026-09-09 18:05:02 +0000

    publish ssh key distribution sync after rotation

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 16:43:09 UTC
+# Last sync: 2026-09-09 18:05:02 UTC
