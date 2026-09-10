281cea18dc3e37b915336f0887333d7aedf84341
James Holloway <james.holloway94@gmail.com>
2026-09-10 18:28:37 +0000

    backfill s3 versioning sync marker in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-10 17:30:44 UTC
+# Last sync: 2026-09-10 18:28:37 UTC
