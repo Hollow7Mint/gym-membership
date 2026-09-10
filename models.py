930b01fe1b40218fda764773798d12bb1ecfe85d
James Holloway <james.holloway94@gmail.com>
2026-09-10 16:34:16 +0000

    persist vpc route table sync after update

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-10 15:27:19 UTC
+# Last sync: 2026-09-10 16:34:16 UTC
