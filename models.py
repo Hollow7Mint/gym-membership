39dffd4d05cf91385c23ca94a9fe0bea0c7e4bed
James Holloway <james.holloway94@gmail.com>
2026-09-11 13:30:14 +0000

    set ssh tunnel sync stamp in config

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 12:56:51 UTC
+# Last sync: 2026-09-11 13:30:14 UTC
