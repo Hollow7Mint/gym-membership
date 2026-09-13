3842ab00a5a70c57aeb7c497b1b886c7ba5bcad6
James Holloway <james.holloway94@gmail.com>
2026-09-13 18:49:03 +0000

    carry bastion config sync stamp to metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-13 18:03:15 UTC
+# Last sync: 2026-09-13 18:49:03 UTC
