58f3df7d4ce4d7d699fa8c6caa5e0c7e1261b24d
James Holloway <james.holloway94@gmail.com>
2026-09-14 23:47:07 +0000

    normalize ecs cluster config time in metadata

--- models.py
+++ models.py
@@ -106,4 +106,4 @@ class GymModels:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-14 22:38:00 UTC
+# Last sync: 2026-09-14 23:47:07 UTC
